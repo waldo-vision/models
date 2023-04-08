d57733b7062e150013f861c8ff3095744767a732
Jared Boggs <jaredb1011@gmail.com>
2023-04-08 12:14:48 -0500

    comply with pylint

--- utils/link_retrieval.py
+++ utils/link_retrieval.py
@@ -1,3 +1,6 @@
+"""
+Requests youtube URLs from waldo api, validates them, and saves to a csv file
+"""
 import argparse
 import json
 import os
@@ -19,14 +22,28 @@ parser.add_argument("-r", "--requirements", help='Specfications for links retrie
 
 args = vars(parser.parse_args())
 
-#regex function to check for valid youtube url, this does not need to be complex because were basically just checking for a video and domain
 def is_valid_youtube_url(url):
-    regex = r'https?://(?:www\.)?(?:youtube|youtu|youtube-nocookie|music\.youtube|gaming\.youtube|studio\.youtube|content\.googleapis|googlevideo)\.(?:com|be)/(?:watch\?v=|embed/|v/|.+\?v=)?([^\&=%\?]+)'
+    """Checks via regex if URL is a valid youtube link"""
+    regex = (
+            r'https?://(?:www\.)?(?:youtube|youtu|youtube-nocookie|music'
+            r'\.youtube|gaming\.youtube|studio\.youtube|content\.googleapis|googlevideo)'
+            r'\.(?:com|be)/(?:watch\?v=|embed/|v/|.+\?v=)?([^\&=%\?]+)'
+            )
     return bool(re.match(regex, url))
 
 def parse_data(data):
-    # Convert the data to a pandas DataFrame, this feels weird, if you find a better solution, please fix.
-    # Were expecting data that looks like this: {"page": 0,"totalPages": 100,"gameplay": [{"uuid": "38r8hnf80ew-35uhehrfnjea-wryhghjvsdk","ytUrl": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"},{"uuid": "38r8hnf80ew-35uhehrfnjea-wryhghjvsdk","ytUrl": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}]}
+    """
+    Convert the data to a pandas DataFrame. If you find a better solution, please fix.
+    Were expecting data that looks like this:
+    {"page": 0,
+    "totalPages": 100,
+    "gameplay": [{"uuid": "38r8hnf80ew-35uhehrfnjea-wryhghjvsdk",
+                  "ytUrl": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"},
+                {"uuid": "38r8hnf80ew-35uhehrfnjea-wryhghjvsdk",
+                 "ytUrl": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}]
+    }
+    """
+
     response_dataframe = pd.DataFrame(data)
     response_list = response_dataframe['gameplay'].tolist()
     response_dataframe = pd.DataFrame(columns=['uuid','url'])
@@ -46,12 +63,16 @@ def parse_data(data):
     return valid_urls
 
 def main(requirements):
-    #Example requirements
-    # {
-    #     "minReviews": 25,
-    #     "rating": 90,
-    # }
-    # Define the API endpoint and parameters
+    """
+    Pulls URLs from the DB that meet the criteria in the requirements argument
+
+    Example requirements
+    {
+        "minReviews": 25,
+        "rating": 90,
+    }
+    """
+
     endpoint = args['endpoint']
     params = {
         "auth_token": args['key'],
@@ -59,7 +80,7 @@ def main(requirements):
     }
     params_json = json.dumps(params)
     # Make the API request and retrieve the data
-    response = requests.get(endpoint, params=params_json)
+    response = requests.get(endpoint, params=params_json, timeout=10)
     data = response.json()
     valid_urls = parse_data(data)
     # Make output directory if it doesn't exist
@@ -70,6 +91,6 @@ def main(requirements):
         valid_urls_df = pd.DataFrame(valid_urls)
         valid_urls_df.to_csv((os.path.join(download_dir + "links.csv")),
                             index=True, columns=["uuid","url"])
-        
+
 if __name__ == "__main__":
     main(dict(args['requirements']))
