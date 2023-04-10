9e05d2ca01b2cc467a49e0f3c588ea94ae82a5b8
yash.parge <yash_parge@yahoo.com.au>
2023-04-10 23:04:00 +1000

    linting changes

--- utils/DefaultVideoCropper.py
+++ utils/DefaultVideoCropper.py
@@ -2,12 +2,17 @@ from typing import Any, Tuple
 import cv2
 from common import ensure_dir_exists
 
+
 class DefaultVideoCropper:
+    """
+    Creates a video cropper object which can crop and save
+    the frames of a video to a specified output directory
+    """
     def __init__(
         self,
         output_dir: str,
         cropping_dimensions: Tuple[int, int],
-        crop_position: Tuple[int, int] = (0, 0)
+        crop_position: Tuple[int, int] = (0, 0),
     ) -> None:
         self._output_dir = output_dir
         ensure_dir_exists(self._output_dir)
@@ -15,17 +20,20 @@ class DefaultVideoCropper:
         self._crop_position_x, self._crop_position_y = crop_position
 
     def _crop_frame(self, video_frame: Any) -> Any:
-        ''' Crops a frame from a video in place'''
-        video_frame = video_frame[self._crop_position_y:self._crop_position_y+self._crop_height, self._crop_position_x:self._crop_position_x+self._crop_width]
+        """Crops a given frame"""
+        video_frame = video_frame[
+            self._crop_position_y : self._crop_position_y + self._crop_height,
+            self._crop_position_x : self._crop_position_x + self._crop_width,
+        ]
         return video_frame
 
     def process_video(self, vid_path: str, video_uuid: str) -> None:
-        '''Reads from the given video path and outputs frames of the video'''
+        """Crops an input video and saves its frames"""
         vidcap = cv2.VideoCapture(vid_path)
         count = 0
         was_read, frame = vidcap.read()
         while was_read:
             frame = self._crop_frame(frame)
-            cv2.imwrite(f'{self._output_dir}/vid_{video_uuid}_frame_{count}.jpg', frame)
+            cv2.imwrite(f"{self._output_dir}/vid_{video_uuid}_frame_{count}.jpg", frame)
             was_read, frame = vidcap.read()
             count += 1
--- utils/crop_videos.py
+++ utils/crop_videos.py
@@ -2,25 +2,42 @@ import argparse
 from DefaultVideoCropper import DefaultVideoCropper
 
 
-parser = argparse.ArgumentParser(description="Crop a video and store frames locally", add_help=False)
-parser.add_argument("-i", "--input", help='Input Video', required=True, type=str)
-parser.add_argument("-w", "--width", help='Cropping width', required=True, type=int)
-parser.add_argument("-h", "--height", help='Cropping height', required=True, type=int)
-parser.add_argument("-o", "--output", help='Folder To Store Output', required=True, type=str)
-parser.add_argument("-x", "--x_position", help='Crop x position', required=False, default=(0), type=int)
-parser.add_argument("-y", "--y_position", help='Crop y position', required=False, default=(0), type=int)
+parser = argparse.ArgumentParser(
+    description="Crop a video and store frames locally", add_help=False
+)
+parser.add_argument("-i", "--input", help="Input Video", required=True, type=str)
+parser.add_argument("-w", "--width", help="Cropping width", required=True, type=int)
+parser.add_argument("-h", "--height", help="Cropping height", required=True, type=int)
+parser.add_argument(
+    "-o", "--output", help="Folder To Store Output", required=True, type=str
+)
+parser.add_argument(
+    "-x", "--x_position", help="Crop x position", required=False, default=(0), type=int
+)
+parser.add_argument(
+    "-y", "--y_position", help="Crop y position", required=False, default=(0), type=int
+)
 
 args = vars(parser.parse_args())
 
-def extract_vid_uuid(vid_path:str) -> str:
+
+def extract_vid_uuid(vid_path: str) -> str:
+    """Extracts uuid from video path"""
     vid_name = vid_path.split("\\")[-1]
     uuid = vid_name.split(".")[0]
     return uuid
 
-def main() -> None:
-    cropper = DefaultVideoCropper(output_dir=args['output'], cropping_dimensions=(args['width'], args['height']), crop_position=(args['x_position'], args['y_position']))
-    uuid = extract_vid_uuid(args['input'])
-    cropper.process_video(args['input'], uuid)
 
-if __name__=="__main__":
+def main() -> None:
+    """Crops an input video and saves its frames"""
+    cropper = DefaultVideoCropper(
+        output_dir=args["output"],
+        cropping_dimensions=(args["width"], args["height"]),
+        crop_position=(args["x_position"], args["y_position"]),
+    )
+    uuid = extract_vid_uuid(args["input"])
+    cropper.process_video(args["input"], uuid)
+
+
+if __name__ == "__main__":
     main()
