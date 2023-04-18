d3ff6dfbf6956316aa9796d8d6c06388b28888db
Joe-TheBro <hammj00@gmail.com>
2023-04-18 20:23:18 +0000

    Updated link-retrieval to work with frontend implementation Co-authored-by: Bensikrac https://github.com/Bensikrac

--- utils/link_retrieval.py
+++ utils/link_retrieval.py
@@ -1,9 +1,4 @@
-"""
-Requests youtube URLs from waldo api that meet requirements
-Validates fetched URLs, and saves them to a csv file
-"""
 import argparse
-import json
 import os
 import pandas as pd
 import requests
@@ -11,89 +6,92 @@ import validators
 from common import ensure_dir_exists
 from pathlib import Path
 
-# Setup command line arguments
+# Set up command line arguments
 parser = argparse.ArgumentParser(description="Get URL's from API and store them locally")
-parser.add_argument("-e", "--endpoint", help='Target URL On Server', required=False,
-                    default="https://waldo.vision/api/trpc/urls", type=str)
+parser.add_argument("-e", "--endpoint", help='Target URL on server', required=False,
+                    default="https://waldo.vision/api/analysis/urls", type=str)
 parser.add_argument("-k", "--key", help='API Key', required=False,
-                    default=(os.environ.get("WALDO_API_KEY")), type=str)
+                    default=os.environ.get("WALDO_API_KEY"), type=str)
 parser.add_argument("-i", "--id", help='API Key ID', required=False, type=str,
-                    default=(os.environ.get("WALDO_API_ID")))
-parser.add_argument("-o", "--output", help='Folder To Store Output', required=True, type=str)
-parser.add_argument("-r", "--requirements", help='Specfications for links retrieved',
-                    default='{"minReviews": 25, "rating": 90}', required=False, type=str)
+                    default=os.environ.get("WALDO_API_ID"))
+parser.add_argument("-o", "--output", help='Folder to store output', required=True, type=str)
+parser.add_argument('--minreviews', help='Minimum number of reviews', required=True, type=int)
+parser.add_argument('--rating', help='Minimum rating', required=True, type=int)
 
 args = vars(parser.parse_args())
 
 def parse_data(data):
     """
-    Convert the data to a pandas DataFrame. If you find a better solution, please fix.
-    We're expecting data that looks like this:
-    {"page": 0,
-    "totalPages": 100,
-    "gameplay": [{"uuid": "38r8hnf80ew-35uhehrfnjea-wryhghjvsdk",
-                  "ytUrl": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"},
-                {"uuid": "38r8hnf80ew-35uhehrfnjea-wryhghjvsdk",
-                 "ytUrl": "https://www.youtube.com/watch?v=dQw4w9WgXcQ"}]
-    }
+    Convert the data to a pandas DataFrame and validate the URLs.
+
+    :param data: A dictionary containing the API response data
+    :return: A pandas DataFrame containing the parsed data
     """
-    print(data)
-    response_dataframe = pd.DataFrame(data)
-    response_list = response_dataframe['gameplay'].tolist()
-    response_dataframe = pd.DataFrame(columns=['id','url','game'])
-    for obj in response_list:
-        obj_dataframe = pd.DataFrame(obj, index=[0])
-        obj_dataframe.rename(columns={"id":"id","ytUrl": "url", "game":"game"}, inplace=True)
-        response_dataframe = pd.concat([response_dataframe, obj_dataframe], ignore_index=True)
+    try:
+        response_dataframe = pd.DataFrame(data)
+        response_list = response_dataframe['gameplay'].tolist()
+        response_dataframe = pd.DataFrame(columns=['id', 'url', 'game'])
 
-    # Filter out duplicate links
-    response_dataframe.drop_duplicates(subset=["url"], inplace=True)
+        for obj in response_list:
+            obj_dataframe = pd.DataFrame(obj, index=[0])
+            obj_dataframe.rename(columns={"id": "id", "ytUrl": "url", "game": "game"}, inplace=True)
+            response_dataframe = pd.concat([response_dataframe, obj_dataframe], ignore_index=True)
 
-    # Validate the URLs
-    for row in response_dataframe['url']:
-        if not (validators.url(row)):
-            return print("Invalid URL: " + row['url'])
-    return response_dataframe
+        # Validate the URLs
+        for row in response_dataframe['url']:
+            if not validators.url(row):
+                return print("Invalid URL: " + row['url'])
 
-# def main(requirements):
+        return response_dataframe
+    except Exception as e:
+        print(f"Error while parsing data: {e}")
+        return pd.DataFrame(columns=['id', 'url', 'game'])
+
 def main():
     """
-    Pulls URLs from the DB that meet the criteria in the requirements argument
+    Pull URLs from the API that meet the criteria specified in the requirements argument.
 
-    Example requirements
-    {
-        "minReviews": 25,
-        "rating": 90,
-    }
+    :param requirements: A dictionary containing the requirements for the URLs
     """
-
     endpoint = args['endpoint']
     params = {
-        # "requirements": requirements,
+        'rating': args['rating'],
+        'minReviews': args['minreviews'],
         "page": 0
     }
     headers = {'authorization': args['key'], 'authorization_id': args['id']}
-    
+
     # Make the API request and retrieve the data
-    response = requests.get(endpoint, params=params, headers=headers, timeout=10)
-    data = response.json()
-    response_dataframe = pd.DataFrame(data)
-    totalPages = response_dataframe['totalPages']
-    valid_urls = pd.DataFrame(columns=['id','url','game'])
-    for page in totalPages:
-        params["page"] = page
+    try:
         response = requests.get(endpoint, params=params, headers=headers, timeout=10)
         data = response.json()
-        pd.concat([valid_urls, parse_data(data)], ignore_index=True)
-    # Make output directory if it doesn't exist
-    download_dir = args['output']
-    ensure_dir_exists(Path(download_dir))
-    # Save the downloaded links to a file
-    valid_urls_df = pd.DataFrame(valid_urls)
-    print(valid_urls_df.columns)
-    valid_urls_df.to_csv((os.path.join(Path(download_dir + "links.csv"))),
-                        index=True, columns=["id","url","game"])
+        print(data)
+        total_pages = data["totalPages"]
 
+        valid_urls = pd.DataFrame(columns=['id', 'url', 'game'])
+        for page in range(0, total_pages + 1):  # Query all pages sequentially
+            params["page"] = page  # Update page number
+            response = requests.get(endpoint, params=params, headers=headers, timeout=10)
+            data = response.json()
+            valid_urls = pd.concat([valid_urls, parse_data(data)], ignore_index=True)
+
+        # Filter out duplicate links
+        valid_urls.drop_duplicates(subset=["url"], inplace=True)
+        download_dir = args['output']
+
+        # Ensure output directory exists
+        ensure_dir_exists(Path(download_dir))
+
+        # Save the downloaded links to a file
+        valid_urls_df = pd.DataFrame(valid_urls)
+        valid_urls_df.to_csv(os.path.join(Path(download_dir), "links.csv"), index=True, columns=["id", "url", "game"])
+    except requests.exceptions.Timeout as timeout_error:
+        print(f"Request timed out: {timeout_error}")
+    except requests.exceptions.TooManyRedirects as redirect_error:
+        print(f"Too many redirects: {redirect_error}")
+    except requests.exceptions.RequestException as request_error:
+        print(f"Request failed: {request_error}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 if __name__ == "__main__":
-    # main(dict(args['requirements']))
     main()
